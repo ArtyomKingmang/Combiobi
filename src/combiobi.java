65de970a6277498d279b7dad02da12e17ae91226
ArtyomKingmang <kingmangapp@gmail.com>
2023-09-22 11:46:30 +0300

    update to Lazurite 2.6 java 19

--- src/combiobi.java
+++ src/combiobi.java
@@ -1,8 +1,14 @@
+import com.kingmang.lazurite.parser.pars.SourceLoader;
+
 import java.io.IOException;
-import static com.kingmang.lazurite.Main.RUN;
+
+import static com.kingmang.lazurite.Main.RunProgram;
+
 
 public class combiobi {
     public static void main(String[] args) throws IOException {
-        RUN("index.lzr");
+        RunProgram(SourceLoader.readSource("index.lzr"), false,false,false, 0);
     }
+
+
 }
